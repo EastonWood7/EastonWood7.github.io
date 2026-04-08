107867148ecb6c4a69b894baf37b196544110d90
EastonWood7 <eastonwood73658@gmail.com>
2026-04-08 17:58:08 +0000

    todo 1 and 2 of sorting

--- asd-projects/sorting/index.js
+++ asd-projects/sorting/index.js
@@ -14,7 +14,17 @@ The CSS ids you will work with are:
 ///////////////////////////////////////////////////////////////////////
 
 // TODO 2: Implement bubbleSort
-
+async function bubbleSort (arr) {
+    for (var i = 0; i < arr.length - 1; i++) {
+        for (var j = arr.length - 1; j > i; j--) {
+            if (arr[j] < arr[j - 1]) {
+                swap(arr, j, j - 1);
+                updateCounter(bubbleCounter);
+                await sleep();
+            }
+        }
+    }
+}
 
 // TODO 3: Implement quickSort
 
@@ -23,7 +33,12 @@ The CSS ids you will work with are:
 
 
 // TODO 1: Implement swap
-
+function swap (arr, i, j) {
+    var temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+    drawSwap(arr, i, j);
+}
 
 ///////////////////////////////////////////////////////////////////////
 /////////////////////// YOUR WORK GOES ABOVE HERE /////////////////////
